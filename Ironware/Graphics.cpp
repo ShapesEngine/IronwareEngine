c58220144e82df33ca39061957583fac7310be79
Yernar Aldabergenov <yernar.aa@gmail.com>
2020-11-16 06:25:23 +0600

    graphics bug fix & windows message bug fix

--- Ironware/Graphics.cpp
+++ Ironware/Graphics.cpp
@@ -78,9 +78,9 @@ Graphics::Graphics( HWND hWnd )
 		nullptr,
 		D3D_DRIVER_TYPE_HARDWARE,
 		nullptr,
-		0,
-		nullptr,
 		swapCreateFlags,
+		nullptr,
+		0,
 		D3D11_SDK_VERSION,
 		&scd,
 		&pSwapChain,
@@ -88,6 +88,22 @@ Graphics::Graphics( HWND hWnd )
 		nullptr,
 		&pImmediateContext 
 	) );
+
+	// create device and front/back buffers, and swap chain and rendering context
+	/*GFX_THROW_INFO( D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		swapCreateFlags,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&sd,
+		&pSwap,
+		&pDevice,
+		nullptr,
+		&pContext
+	) );*/
 
 	// gain access to texture subresource in swap chain (back buffer)
 	ID3D11Resource* pBackBuffer = nullptr;
@@ -180,7 +196,7 @@ const char* Graphics::HrException::what() const noexcept
 
 std::wstring Graphics::HrException::GetErrorDescription() const noexcept
 {
-	wchar_t buf[512];
+	wchar_t buf[512];	
 	DXGetErrorDescription( hr, buf, sizeof( buf ) );
 	return buf;
 }
--- Ironware/WinMain.cpp
+++ Ironware/WinMain.cpp
@@ -12,14 +12,17 @@ int CALLBACK WinMain(
 	}
 	catch( const IronException& e )
 	{
+		SetWindowLongPtr( GetActiveWindow(), GWLP_WNDPROC, reinterpret_cast<LONG_PTR>( &DefWindowProc ) );
 		MessageBox( nullptr, WCON_CHREINT_CAST( e.what() ), WCON_CHREINT_CAST( e.GetType() ), MB_OK | MB_ICONEXCLAMATION );
 	}
 	catch( const std::exception& e )
 	{
+		SetWindowLongPtr( GetActiveWindow(), GWLP_WNDPROC, reinterpret_cast<LONG_PTR>( &DefWindowProc ) );
 		MessageBox( nullptr, WCON_CHREINT_CAST( e.what() ), L"Standard Exception", MB_OK | MB_ICONEXCLAMATION );
 	}
 	catch( ... )
 	{
+		SetWindowLongPtr( GetActiveWindow(), GWLP_WNDPROC, reinterpret_cast<LONG_PTR>( &DefWindowProc ) );
 		MessageBox( nullptr, L"No details available", L"Unknown Exception", MB_OK | MB_ICONEXCLAMATION );
 	}
 	return -1;
