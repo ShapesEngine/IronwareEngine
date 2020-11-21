7c045cb5b7a2b5b89b06895427bb3935fc01c062
Yernar Aldabergenov <yernar.aa@gmail.com>
2020-11-21 18:20:27 +0600

    message box flag change

--- Ironware/WinMain.cpp
+++ Ironware/WinMain.cpp
@@ -13,15 +13,15 @@ int CALLBACK wWinMain(
 	}
 	catch( const IronException& e )
 	{
-		SAFE_MESSAGEBOX( nullptr, CON_WCHREINT_CAST( e.what() ), e.GetType(), MB_OK | MB_ICONEXCLAMATION );
+		SAFE_MESSAGEBOX( nullptr, CON_WCHREINT_CAST( e.what() ), e.GetType(), MB_OK | MB_ICONERROR );
 	}
 	catch( const std::exception& e )
 	{
-		SAFE_MESSAGEBOX( nullptr, ToWide( e.what() ).c_str(), L"Standard Exception", MB_OK | MB_ICONEXCLAMATION );
+		SAFE_MESSAGEBOX( nullptr, ToWide( e.what() ).c_str(), L"Standard Exception", MB_OK | MB_ICONERROR );
 	}
 	catch( ... )
 	{
-		SAFE_MESSAGEBOX( nullptr, L"No details available", L"Unknown Exception", MB_OK | MB_ICONEXCLAMATION );
+		SAFE_MESSAGEBOX( nullptr, L"No details available", L"Unknown Exception", MB_OK | MB_ICONERROR );
 	}
 	return -1;
 }
