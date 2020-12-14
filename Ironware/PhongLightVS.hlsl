15456f853fbe255353398bb3433578006e35f46f
Yernar Aldabergenov <yernar.aa@gmail.com>
2020-12-14 21:22:36 +0600

    specular relative to the view pos

--- Ironware/PhongLightVS.hlsl
+++ Ironware/PhongLightVS.hlsl
@@ -1,12 +1,12 @@
 cbuffer CBuffer
 {
-    matrix model;
+    matrix modelView;
     matrix modelViewProjection;
 };
 
 struct VSOut
 {
-    float3 worldPos : Position;
+    float3 viewPos : Position;
     float3 normal : Normal;
     float4 pos : SV_Position;
 };
@@ -14,8 +14,8 @@ struct VSOut
 VSOut main(float3 pos : Position, float3 n : Normal)
 {
     VSOut vso;
-    vso.worldPos = (float3) mul(float4(pos, 1.f), model);
-    vso.normal = mul(n, (float3x3) model);
+    vso.viewPos = (float3) mul(float4(pos, 1.f), modelView);
+    vso.normal = mul(n, (float3x3) modelView);
     vso.pos = mul(float4(pos, 1.f), modelViewProjection);
     return vso;
 }
