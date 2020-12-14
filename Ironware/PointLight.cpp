f1fac2a68c9da9170e817911dba5cae91b2162be
Yernar Aldabergenov <yernar.aa@gmail.com>
2020-12-14 13:58:38 +0600

    moving constants to cbuf to be able to change in future

--- Ironware/PointLight.cpp
+++ Ironware/PointLight.cpp
@@ -12,16 +12,18 @@
 PointLight::PointLight( Graphics& gfx, float radius ) :
 	mesh( gfx, radius ),
 	cbuffer( gfx )
-{}
+{
+	Reset();
+}
 
 void PointLight::SpawnControlWindow() noexcept
 {
 	if( ImGui::Begin( "Light" ) )
 	{
 		ImGui::Text( "Position" );
-		ImGui::SliderFloat( "X", &pos.x, -60.f, 60.f, "%.1f" );
-		ImGui::SliderFloat( "Y", &pos.y, -60.f, 60.f, "%.1f" );
-		ImGui::SliderFloat( "Z", &pos.z, -60.f, 60.f, "%.1f" );
+		ImGui::SliderFloat( "X", &cbufData.pos.x, -60.f, 60.f, "%.1f" );
+		ImGui::SliderFloat( "Y", &cbufData.pos.y, -60.f, 60.f, "%.1f" );
+		ImGui::SliderFloat( "Z", &cbufData.pos.z, -60.f, 60.f, "%.1f" );
 		if( ImGui::Button( "Reset" ) )
 		{
 			Reset();
@@ -32,7 +34,26 @@ void PointLight::SpawnControlWindow() noexcept
 
 void PointLight::Draw( Graphics& gfx ) const noexcept( !IS_DEBUG )
 {
-	mesh.SetPosition( pos );
-	mesh.Draw( gfx );
+	mesh.SetPosition( cbufData.pos );
+	mesh.Draw( gfx );	
+}
+
+void PointLight::Reset() noexcept
+{
+	cbufData = {
+		{ 0.0f,0.0f,0.0f },
+		{ 0.7f,0.7f,0.9f },
+		{ 0.05f,0.05f,0.05f },
+		{ 1.0f,1.0f,1.0f },
+		1.0f,
+		1.0f,
+		0.045f,
+		0.0075f,
+	};
+}
+
+void PointLight::Bind( Graphics& gfx ) const noexcept
+{ 
+	cbuffer.Update( gfx, cbufData );
 	cbuffer.Bind( gfx );
 }
--- Ironware/PointLight.h
+++ Ironware/PointLight.h
@@ -23,19 +23,25 @@ public:
 	PointLight( Graphics& gfx, float radius = 0.5f );
 	void SpawnControlWindow() noexcept;	
 	void Draw( Graphics& gfx ) const noexcept( !IS_DEBUG );
-	void Reset() noexcept { pos = { 0.f, 0.f, 0.f }; }
-	void Bind( Graphics& gfx ) const noexcept { cbuffer.Update( gfx,PointLightCBuf{ pos } ); }
+	void Reset() noexcept;	
+	void Bind( Graphics& gfx ) const noexcept;
 
 private:
 	struct PointLightCBuf
 	{
-		DirectX::XMFLOAT3 pos;
-		// to make the struct's size 16 byte
-		float padding;
+		// GPU expects 16 byte val
+		alignas( 16 ) DirectX::XMFLOAT3 pos;
+		alignas( 16 ) DirectX::XMFLOAT3 materialColor;
+		alignas( 16 ) DirectX::XMFLOAT3 ambient;
+		alignas( 16 ) DirectX::XMFLOAT3 diffuseColor;
+		float diffuseIntensity;
+		float attConst;
+		float attLin;
+		float attQuad;
 	};
 
 private:
-	DirectX::XMFLOAT3 pos = { 0.f, 0.f, 0.f };
+	PointLightCBuf cbufData;
 	mutable SolidSphere mesh;
 	mutable PixelConstantBuffer<PointLightCBuf> cbuffer;
 };
