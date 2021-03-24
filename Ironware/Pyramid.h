66da74af832faba4f96c4a6d24d8589932dd06b1
Yernar <yernar.aa@gmail.com>
2021-03-25 00:15:12 +0600

    pyramid(cone) independent faces generation

--- Ironware/Pyramid.h
+++ Ironware/Pyramid.h
@@ -81,5 +81,75 @@ public:
 	}
 
 	template<class V>
+	static IndexedTriangleList<V> MakeTesselatedIndependentFaces( int longDiv )
+	{
+		namespace dx = DirectX;
+
+		assert( longDiv >= 3 );
+
+		const auto base = dx::XMVectorSet( 1.f, 0.f, -1.f, 0.f );
+		const float longitudeAngle = 2.f * PI / longDiv;
+
+		std::vector<V> vertices;
+
+		// cone tip + edge vertices(no center base)
+		for( int iLong = 0; iLong < longDiv; iLong++ )
+		{
+			// tip
+			vertices.emplace_back();
+			vertices.back().pos = { 0.f, 0.f, 1.f };
+
+			const float angles[] = {
+				longitudeAngle * iLong,
+				longitudeAngle * ( ( iLong + 1 ) != longDiv ? ( iLong + 1 ) : 0 ),
+			};
+
+			for( auto a : angles )
+			{
+				vertices.emplace_back();
+				auto v = dx::XMVector3Transform(
+					base,
+					dx::XMMatrixRotationZ( a )
+				);
+				dx::XMStoreFloat3( &vertices.back().pos, v );
+			}
+		}
+
+		const auto iBaseCenter = (uint32_t)vertices.size();
+		vertices.emplace_back();
+		vertices.back().pos = { 0.f, 0.f, -1.f };
+
+		const auto iBaseEdges = (uint32_t)vertices.size();
+		// basecenter and edge vertices
+		for( int iLong = 0; iLong < longDiv; iLong++ )
+		{
+			vertices.emplace_back();
+			auto v = dx::XMVector3Transform(
+				base,
+				dx::XMMatrixRotationZ( longitudeAngle * iLong )
+			);
+			dx::XMStoreFloat3( &vertices.back().pos, v );
+		}
+
+		std::vector<uint16_t> indices;
+
+		// cone tip + edge indices
+		for( int iLong = 0; iLong < longDiv * 3; iLong++ )
+		{
+			indices.push_back( iLong );
+		}
+
+		// cone base center + edge indices
+		for( int iLong = 0; iLong < longDiv; iLong++ )
+		{
+			indices.push_back( iBaseCenter );
+			indices.push_back( ( iLong + 1 ) % longDiv + iBaseEdges );
+			indices.push_back( iLong + iBaseEdges );
+		}
+
+		return { std::move( vertices ), std::move( indices ) };
+	}
+
+	template<class V>
 	__forceinline static IndexedTriangleList<V> Make() { return MakeTesselated<V>( 24 ); }
 };
