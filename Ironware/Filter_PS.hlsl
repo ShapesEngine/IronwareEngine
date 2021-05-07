af02873b1e1ce545c7cca05f808bf62fcfdb26ee
Yernar <yernar.aa@gmail.com>
2021-05-07 06:25:33 +0600

    improvements to bluring with max color

--- Ironware/Filter_PS.hlsl
+++ Ironware/Filter_PS.hlsl
@@ -1,8 +1,8 @@
 Texture2D tex;
 SamplerState splr;
 
-static const int r = 7;
-static const float divisor = (2 * r + 1) * (2 * r + 1);
+static const int r = 12;
+static const float divisor = ( 2 * r + 1 ) * ( 2 * r + 1 );
 
 float4 main( float2 uv : TEXCOORD ) : SV_TARGET
 {
@@ -10,15 +10,18 @@ float4 main( float2 uv : TEXCOORD ) : SV_TARGET
     tex.GetDimensions( width, height );
     const float dx = 1.f / (float)width;
     const float dy = 1.f / (float)height;
-    float4 acc = float4( 0.f, 0.f, 0.f, 0.f );
+    float accAlpha = 0.0f;
+    float3 maxColor = float3( 0.0f, 0.0f, 0.0f );
     for( int y = -r; y <= r; y++ )
     {
         for( int x = -r; x <= r; x++ )
         {
             const float2 tc = uv + float2( dx * x, dy * y );
-            acc += tex.Sample( splr, tc );
+            const float4 s = tex.Sample( splr, tc ).rgba;
+            accAlpha += s.a;
+            maxColor = max( s.rgb, maxColor );
         }
     }
     
-    return acc / divisor;
+    return float4( maxColor, accAlpha / divisor );
 }
